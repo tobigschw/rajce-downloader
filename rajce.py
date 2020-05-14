8c384ed5dad3b08c7cd1cb3aa1dd39fe06fd76ef
Vapauden Kuolemasta <kuolemasta@mail.ru>
2020-05-14 14:49:14 +0300

    New download method

--- rajce.py
+++ rajce.py
@@ -9,6 +9,11 @@ import urllib.parse
 import xml.etree.ElementTree as ET
 from pathlib import Path
 from multiprocessing.dummy import Pool
+# for reporthook
+import sys
+import time
+import datetime
+from time import gmtime, strftime
 
 
 class Rajce:
@@ -18,12 +23,14 @@ class Rajce:
     securityCode = None
     storage = None
     filePath = None
+    links = {}
 
-    THREADS_COUNT = 15
+    THREADS_COUNT = 10
 
     def __init__(self, urls, path=None):
         self.urls = urls
-        self.path = path
+
+        self.path = Path(path) if path else Path(__file__).resolve().parent
 
         self.setLogger()
 
@@ -56,128 +63,85 @@ class Rajce:
             self.logger.error(f'Error : "{e.reason}" for url : {url}')
             return {}
 
-        m = re.search('var storage = \"(.+?)\"', response)
-        storage = m.group(1).replace("\\", "") if m else ''
-        if storage == '':
-            self.logger.error(f'Error : "Can\'t parse storage url path"')
+        config = {}
+        for line in response.splitlines(True):
+            m = re.search('var (.+?) = (.+?);$', line.strip('\n\t\r '))
+            if not m or m.group(1) in config: continue
+            config[m.group(1)] = m.group(2).strip('"').replace("\\","")
+
+        if not all(k in config for k in ('albumUserName','albumServerDir','storage')):
+            self.logger.error(f'Error : Some config keys not found')
             return {}
 
-        photos = {}
-        for line in response.splitlines(True):
-            m = re.search('var photos = (.+);$', line.strip('\n\t\r '))
-            if not m: continue
-            photos = json.loads(m.group(1))
-            break
+        user, album, storage = config['albumUserName'], config['albumServerDir'], config['storage']
 
-        return {elem['fileName']: elem['videoStructure']['items'][1]['video'][0]['file']
-        if elem['videoStructure'] else storage + 'images/' + elem['fileName'] for elem in photos}
+        if 'photos' not in config:
+            self.logger.error(f'Error : {user}\'s album "{album}" is empty or password protected')
+            return {}
 
-    def getAlbumList(self, url) -> dict:
+        photos = json.loads(config['photos'])
+        links_dict = {
+            self.path.joinpath(user, album, elem['fileName']): elem['videoStructure']['items'][1]['video'][0]['file']
+            if elem['videoStructure'] else storage + 'images/' + elem['fileName'] for elem in photos
+        }
+
+        # Create album folder
+        if len(links_dict) > 0:
+            try:
+                self.path.joinpath(user, album).mkdir(parents=True, exist_ok=True)
+            except OSError as e:
+                self.logger.error(f'Error "{e}" for mkdir "{user}/{album}"')
+                return {}
+            self.logger.info(f"{len(links_dict)} media files found in {user}'s album '{album}'")
+        else:
+            self.logger.info(f"No media files found in {user}'s album '{album}'")
+
+        return links_dict
+
+    def getAlbumsList(self, url) -> list:
         try:
             body = urllib.request.urlopen(url + '?rss=news').read()
         except urllib.error.URLError as e:
             self.logger.error(f'Error : "{e.reason}" for url : {url}')
-            return {}
+            return []
 
         root = ET.fromstring(body)
 
-        return {x.find('title').text.split(' | ')[1] : x.find('link').text for x in root.findall('channel/item')}
+        return [x.text for x in root.findall('channel/item/link')]
 
     def download(self):
         for url in self.urls:
             path = urllib.parse.urlparse(url).path
             if len(path.strip('/')) > 0:
-                print('getMediaLinks - ' + url)
+                self.links.update(self.getMediaLinks(url))
             else:
-                print('getAlbumList - ' + url)
+                album_urls = self.getAlbumsList(url)
+                for album_url in album_urls:
+                    self.links.update(self.getMediaLinks(album_url))
 
-    def download_gallery(self, galleryUrl):
-        html = urllib.request.urlopen(galleryUrl).read().decode('utf-8')
-
-        pageList = [int(x) for x in re.findall('sort=\w+?&page=(\d+)', html)]
-        sortType = re.search('sort=(\w+?)&page=\d+', html).group(1)
-        page = int(max(pageList)) if pageList else 0
-
-        user = re.search('([\w-]+?)\.rajce\.idnes\.cz/', galleryUrl).group(1)
-
-        while page >= 0:
-            regex = '(' + user + '\.rajce\.idnes\.cz/.*?/)\">'
-            albums = re.findall(regex, html)
-            if not albums:
-                print(galleryUrl + ' - No albums found!')
-                return
-
-            for album in albums:
-                self.download_album(self.correct_url(album))
-
-            if page > 0:
-                pageUrl = galleryUrl + '?listType=&sort=' + sortType + '&page=' + str(page)
-                html = urllib.request.urlopen(pageUrl).read().decode('utf-8')
-
-            page = page - 1
-
-    def download_album(self, albumUrl):
-        html = urllib.request.urlopen(albumUrl).read().decode('utf-8').replace("\\", "")
-
-        storage = re.search('var storage = "(.*)";', html)
-        if not storage:
-            print(albumUrl + ' - No photo found or locked album!')
-            return
-
-        self.videoStorage = None
-        self.storage = storage.group(1)
-        self.securityCode = re.search('var albumSecurityCode = \"(.*)\";', html).group(1)
-
-        links = re.findall('{(\"photoID\".*?)}', html)
-        if not links:
-            print(albumUrl + ' - No photo found!')
-            return
-
-        m = re.search('([\w-]+?)\.rajce\.idnes\.cz/(.*?)/', albumUrl)
-        self.filePath = os.path.join(self.path if self.path else '', m.group(1), m.group(2).replace('.', '_'))
-        os.makedirs(self.filePath, exist_ok=True)
-
+        self.logger.info('Start')
         pool = Pool(self.THREADS_COUNT)
-        pool.map(self.download_file, links)
+        pool.map(self.downloadFile, self.links.keys())
         pool.close()
         pool.join()
+        self.logger.info('Finish')
 
-        print(albumUrl + ' - OK!')
-
-    def download_file(self, fileUrl):
-        if re.search('\"isVideo\":(.*),\"desc', fileUrl).group(1) == 'false':
-            fileName = re.search('\"fileName\":\"(.+?)\"', fileUrl).group(1)
-            fileUrl = self.storage + 'images/' + fileName
-        else:
-            fileName = re.search('\"info\":\"(.+?\..+?)[\s\"]', fileUrl).group(1)
-            photoID = re.search('\"photoID\":\"(\d+)', fileUrl).group(1)
-            if not self.videoStorage:
-                self.get_video_storage(photoID)
-            fileUrl = self.videoStorage + photoID
-
+    def downloadFile(self, file):
+        url = self.links[file]
+        print(f'Downloading "{url}"')
         try:
-            urllib.request.urlretrieve(fileUrl, os.path.join(self.filePath, fileName))
+            urllib.request.urlretrieve(url, file)
+        except urllib.error.HTTPError as e:
+            self.logger.error(f'HTTPError : "{e.reason}" for url : {url}')
+            return False
+        except urllib.error.ContentTooShortError as e:
+            self.logger.error(f'ContentTooShortError : "{e.reason}" for url : {url}')
+            return False
         except urllib.error.URLError as e:
-            print("Can't receive " + fileName + " '" + fileUrl + "' with " + e.reason)
+            self.logger.error(f'URLError : "{e.reason}" for url : {url}')
+            return False
 
-    def get_video_storage(self, photoId):
-        videoUrl = 'https://www.rajce.idnes.cz/ajax/videoxml.php?id=' + photoId + '/' + self.securityCode
-        xml = urllib.request.urlopen(videoUrl).read()
-        root = ET.fromstring(xml)
-        server = root.find('items').find('item').find('linkvideo').find('server').text
-        path = root.find('items').find('item').find('linkvideo').find('path').text
-        self.videoStorage = server + path
-
-    def correct_url(self, url):
-        url = urllib.parse.quote_plus(url.replace("\"", ""), ':/&?=')
-
-        if url[0:8] != 'https://' and url[0:7] != 'http://':
-            url = 'https://' + url
-
-        if url[-1] != '/' and url.find("?") == -1:
-            url = url + '/'
-
-        return url
+        return True
 
 
 if __name__ == '__main__':
